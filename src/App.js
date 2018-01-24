05d513bc46889b3ba6db5de8045f1a07478bbe50
Ashish Shetty <Ashish@Ashishs-MacBook-Pro.local>
2018-01-24 16:32:45 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          this is my user page
+          this is my user
         </p>
       </div>
     );
